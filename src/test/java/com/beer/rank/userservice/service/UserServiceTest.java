9f4a71ccae66e1c769b16a8604535634c04dbe1d
Kristiyan Georgiev <kristiyang@vmware.com>
2019-09-26 14:01:52 +0300

    Update travis and Ignore tests

--- src/test/java/com/beer/rank/userservice/service/UserServiceTest.java
+++ src/test/java/com/beer/rank/userservice/service/UserServiceTest.java
@@ -1,5 +1,6 @@
 package com.beer.rank.userservice.service;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -8,5 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(locations = { "classpath:application-test.properties" })
+@Ignore
 public class UserServiceTest {
 }
