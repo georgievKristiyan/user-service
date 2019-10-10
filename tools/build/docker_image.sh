9276200e23ca92d50b84cd989e05c9709b6bca14
Kristiyan Georgiev <kristiyang@vmware.com>
2019-10-10 15:24:29 +0300

    Add rest client envs for testing

--- tools/build/docker_image.sh
+++ tools/build/docker_image.sh
@@ -3,3 +3,5 @@
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker build -t kristiyang/hackconf:user-service-$TRAVIS_BUILD_NUMBER ./server
 docker push kristiyang/hackconf:user-service-$TRAVIS_BUILD_NUMBER
+docker tag kristiyang/hackconf:user-service-$TRAVIS_BUILD_NUMBER kristiyang/hackconf:user-service-latest
+docker push kristiyang/hackconf:user-service-latest
